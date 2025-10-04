dd21782fc884017d2f95b0e03a97905b15e087ea
DanOrsborne <54726325+DanOrsborne@users.noreply.github.com>
2025-10-04 08:29:33 +0100

    Sync update

--- frontend/src/EventCheckInPage.jsx
+++ frontend/src/EventCheckInPage.jsx
@@ -53,6 +53,13 @@ const EventCheckInPage = () => {
     Promise.all([fetchEvent(), fetchRegistrations(eventId)]).finally(() => setLoading(false));
   }, [eventId, navigate]);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetchRegistrations(eventId);
+    }, 10000); // every 10 seconds
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, [eventId]);
 
 
   const fetchRegistrations = async (eventId) => {
@@ -135,7 +142,7 @@ const EventCheckInPage = () => {
 
         {!fromSessionStorage && <Button className='no-print' variant="contained" sx={{ mb: 3 }} onClick={() => navigate(-1)}>Back</Button>}
         {fromSessionStorage && <Button className='no-print' variant="contained" sx={{ mb: 3 }} onClick={() => handleHelperLogout()}>Logout</Button>}
-        {!fromSessionStorage && <Button className='no-print' variant="contained" sx={{ml:1, mb: 3 }} onClick={() => navigate(`/checkin/${eventId}/helperlogin`)}>Share Helper Login</Button>}
+        {!fromSessionStorage && <Button className='no-print' variant="contained" sx={{ ml: 1, mb: 3 }} onClick={() => navigate(`/checkin/${eventId}/helperlogin`)}>Share Helper Login</Button>}
 
 
         <GDPRNotice />
@@ -144,93 +151,93 @@ const EventCheckInPage = () => {
 
         <Typography variant="h5" sx={{ mb: 2 }}>Check In {event ? ` - ${event.EventName}` : ''}</Typography>
 
- {!event.Active && (<Typography variant="h5" sx={{ mb: 2 }}>No available for inactive events</Typography>)}
-{event.Active && (<>
-        <Box sx={{ display: 'flex', gap: 2, mb: 3 }}>
-          <TextField
-            label="Filter by Parent Name"
-            variant="outlined"
-            size="small"
-            value={parentFilter}
-            onChange={(e) => setParentFilter(e.target.value)}
-          />
-          <TextField
-            label="Filter by Child Name"
-            variant="outlined"
-            size="small"
-            value={childFilter}
-            onChange={(e) => setChildFilter(e.target.value)}
-          />
+        {!event.Active && (<Typography variant="h5" sx={{ mb: 2 }}>No available for inactive events</Typography>)}
+        {event.Active && (<>
+          <Box sx={{ display: 'flex', gap: 2, mb: 3 }}>
+            <TextField
+              label="Filter by Parent Name"
+              variant="outlined"
+              size="small"
+              value={parentFilter}
+              onChange={(e) => setParentFilter(e.target.value)}
+            />
+            <TextField
+              label="Filter by Child Name"
+              variant="outlined"
+              size="small"
+              value={childFilter}
+              onChange={(e) => setChildFilter(e.target.value)}
+            />
 
-          <Select onChange={(e) => setClassFilter(e.target.value)}
-            value={classFilter}
-            size="small"
-            label="Filter by Class"
-            variant="outlined">
-            <MenuItem value="*">All</MenuItem>
-            {[...new Set(registrations.map((reg) => reg.EventQuestion2Answer).filter(Boolean))]
-              .sort((a, b) => a.localeCompare(b))
-              .map((className, idx) => (
-                <MenuItem key={idx} value={className}>
-                  {className}
-                </MenuItem>
-              ))}
-          </Select>
+            <Select onChange={(e) => setClassFilter(e.target.value)}
+              value={classFilter}
+              size="small"
+              label="Filter by Class"
+              variant="outlined">
+              <MenuItem value="*">All</MenuItem>
+              {[...new Set(registrations.map((reg) => reg.EventQuestion2Answer).filter(Boolean))]
+                .sort((a, b) => a.localeCompare(b))
+                .map((className, idx) => (
+                  <MenuItem key={idx} value={className}>
+                    {className}
+                  </MenuItem>
+                ))}
+            </Select>
 
-          <Button disabled={
-            !parentFilter &&
-            !childFilter &&
-            (!classFilter || classFilter === "*") // disable if empty or "*"
-          } variant="contained" sx={{ mt: 0 }} onClick={() => clearFilters()}>Clear</Button>
-        </Box>
+            <Button disabled={
+              !parentFilter &&
+              !childFilter &&
+              (!classFilter || classFilter === "*") // disable if empty or "*"
+            } variant="contained" sx={{ mt: 0 }} onClick={() => clearFilters()}>Clear</Button>
+          </Box>
 
-        <Table >
-          <TableHead>
-            <TableRow>
-              <TableCell sx={{ minWidth: 50 }} >Check In/Out</TableCell>
-              <TableCell>Parent Details</TableCell>
+          <Table >
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ minWidth: 50 }} >Check In/Out</TableCell>
+                <TableCell>Parent Details</TableCell>
 
-              <TableCell>{event.EventQuestion1Name}</TableCell>
-              <TableCell>{event.EventQuestion2Name}</TableCell>
-              <TableCell>{event.EventQuestion3Name}</TableCell>
-              <TableCell>{event.EventQuestion4Name}</TableCell>
-              <TableCell>{event.EventQuestion5Name}</TableCell>
-              <TableCell>{event.EventQuestion6Name}</TableCell>
-              <TableCell>{event.EventQuestion7Name}</TableCell>
-              <TableCell>{event.EventQuestion8Name}</TableCell>
-              <TableCell>{event.EventQuestion9Name}</TableCell>
-              <TableCell>{event.EventQuestion10Name}</TableCell>
-
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {filteredRegistrations.sort((a, b) => a.EventQuestion1Answer.localeCompare(b.EventQuestion1Answer)).map((reg) => (
-              <TableRow key={reg.id}>
-                <TableCell ><>{reg.checkedIn ? <CheckBoxIcon sx={{ color: 'green' }} onClick={() => handleCheckInOut(reg.CustomerId, "checkedIn", false, true)} />
-                  : <CheckBoxOutlineBlankIcon onClick={() => handleCheckInOut(reg.CustomerId, "checkedIn", true, false)} />}
-                  {reg.checkedOut ? <DisabledByDefaultIcon sx={{ color: 'red' }} onClick={() => handleCheckInOut(reg.CustomerId, "checkedOut", false, true)} />
-                    : <CheckBoxOutlineBlankIcon onClick={() => handleCheckInOut(reg.CustomerId, "checkedOut", true, false, reg.checkedIn)} />}</>
-                </TableCell>
-                <TableCell>{reg.ParentName}<br />
-                  {reg.ParentEmail}<br />
-                  {reg.ParentMobile}</TableCell>
-
-                <TableCell>{event.EventQuestion1Id == reg.EventQuestion1Name ? reg.EventQuestion1Answer : ""}</TableCell>
-                <TableCell>{event.EventQuestion2Id == reg.EventQuestion2Name ? reg.EventQuestion2Answer : ""}</TableCell>
-                <TableCell>{event.EventQuestion3Id == reg.EventQuestion3Name ? reg.EventQuestion3Answer : ""}</TableCell>
-                <TableCell>{event.EventQuestion4Id == reg.EventQuestion4Name ? reg.EventQuestion4Answer : ""}</TableCell>
-                <TableCell>{event.EventQuestion5Id == reg.EventQuestion5Name ? reg.EventQuestion5Answer : ""}</TableCell>
-                <TableCell>{event.EventQuestion6Id == reg.EventQuestion6Name ? reg.EventQuestion6Answer : ""}</TableCell>
-                <TableCell>{event.EventQuestion7Id == reg.EventQuestion7Name ? reg.EventQuestion7Answer : ""}</TableCell>
-                <TableCell>{event.EventQuestion8Id == reg.EventQuestion8Name ? reg.EventQuestion8Answer : ""}</TableCell>
-                <TableCell>{event.EventQuestion9Id == reg.EventQuestion9Name ? reg.EventQuestion9Answer : ""}</TableCell>
-                <TableCell>{event.EventQuestion10Id == reg.EventQuestion10Name ? reg.EventQuestion10Answer : ""}</TableCell>
+                <TableCell>{event.EventQuestion1Name}</TableCell>
+                <TableCell>{event.EventQuestion2Name}</TableCell>
+                <TableCell>{event.EventQuestion3Name}</TableCell>
+                <TableCell>{event.EventQuestion4Name}</TableCell>
+                <TableCell>{event.EventQuestion5Name}</TableCell>
+                <TableCell>{event.EventQuestion6Name}</TableCell>
+                <TableCell>{event.EventQuestion7Name}</TableCell>
+                <TableCell>{event.EventQuestion8Name}</TableCell>
+                <TableCell>{event.EventQuestion9Name}</TableCell>
+                <TableCell>{event.EventQuestion10Name}</TableCell>
 
               </TableRow>
-            ))}
-          </TableBody>
-        </Table></>
-)}
+            </TableHead>
+            <TableBody>
+              {filteredRegistrations.sort((a, b) => a.EventQuestion1Answer.localeCompare(b.EventQuestion1Answer)).map((reg) => (
+                <TableRow key={reg.id}>
+                  <TableCell ><>{reg.checkedIn ? <CheckBoxIcon sx={{ color: 'green' }} onClick={() => handleCheckInOut(reg.CustomerId, "checkedIn", false, true)} />
+                    : <CheckBoxOutlineBlankIcon onClick={() => handleCheckInOut(reg.CustomerId, "checkedIn", true, false)} />}
+                    {reg.checkedOut ? <DisabledByDefaultIcon sx={{ color: 'red' }} onClick={() => handleCheckInOut(reg.CustomerId, "checkedOut", false, true)} />
+                      : <CheckBoxOutlineBlankIcon onClick={() => handleCheckInOut(reg.CustomerId, "checkedOut", true, false, reg.checkedIn)} />}</>
+                  </TableCell>
+                  <TableCell>{reg.ParentName}<br />
+                    {reg.ParentEmail}<br />
+                    {reg.ParentMobile}</TableCell>
+
+                  <TableCell>{event.EventQuestion1Id == reg.EventQuestion1Name ? reg.EventQuestion1Answer : ""}</TableCell>
+                  <TableCell>{event.EventQuestion2Id == reg.EventQuestion2Name ? reg.EventQuestion2Answer : ""}</TableCell>
+                  <TableCell>{event.EventQuestion3Id == reg.EventQuestion3Name ? reg.EventQuestion3Answer : ""}</TableCell>
+                  <TableCell>{event.EventQuestion4Id == reg.EventQuestion4Name ? reg.EventQuestion4Answer : ""}</TableCell>
+                  <TableCell>{event.EventQuestion5Id == reg.EventQuestion5Name ? reg.EventQuestion5Answer : ""}</TableCell>
+                  <TableCell>{event.EventQuestion6Id == reg.EventQuestion6Name ? reg.EventQuestion6Answer : ""}</TableCell>
+                  <TableCell>{event.EventQuestion7Id == reg.EventQuestion7Name ? reg.EventQuestion7Answer : ""}</TableCell>
+                  <TableCell>{event.EventQuestion8Id == reg.EventQuestion8Name ? reg.EventQuestion8Answer : ""}</TableCell>
+                  <TableCell>{event.EventQuestion9Id == reg.EventQuestion9Name ? reg.EventQuestion9Answer : ""}</TableCell>
+                  <TableCell>{event.EventQuestion10Id == reg.EventQuestion10Name ? reg.EventQuestion10Answer : ""}</TableCell>
+
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table></>
+        )}
 
         {!fromSessionStorage && <Button className='no-print' variant="contained" sx={{ mt: 3 }} onClick={() => navigate(-1)}>Back</Button>}
       </Box>
